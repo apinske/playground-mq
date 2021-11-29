b41fa5414f7ef962b104f070331195fafc82a288
Alexander Pinske <alex@pinske.eu>
2021-11-29 21:11:56 +0100

    cleanup

--- src/main/java/eu/pinske/playground/playgroundmq/PlaygroundMqApplication.java
+++ src/main/java/eu/pinske/playground/playgroundmq/PlaygroundMqApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlaygroundMqApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlaygroundMqApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundMqApplication.class, args);
+    }
 
 }
