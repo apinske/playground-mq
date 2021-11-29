8a2892b050332565a7ca26a327f7baa5a6d72b88
Alexander Pinske <alex@pinske.eu>
2021-11-29 20:16:11 +0100

    spring init

---
+++ src/main/java/eu/pinske/playground/playgroundmq/PlaygroundMqApplication.java
@@ -0,0 +1,13 @@
+package eu.pinske.playground.playgroundmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/pinske/playground/playgroundmq/PlaygroundMqApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.pinske.playground.playgroundmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
